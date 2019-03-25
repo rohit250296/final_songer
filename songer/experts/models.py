bececa60044df3ed80d5959ffd841d2ae548e5f1
Thanos250296 <rohitrnair25@gmail.com>
2019-03-26 00:37:07 +0530

    Created model ExpertReview

--- songer/experts/models.py
+++ songer/experts/models.py
@@ -10,3 +10,13 @@ class experts(models.Model):
     password = models.CharField(max_length=30)
     def __str__(self):
         return self.username
+
+class ExpertReview(models.Model):
+    expert_review_id = models.IntegerField(primary_key=True)
+    review = models.CharField(max_length=2000)
+    pat_id = models.ForeignKey(experts,on_delete=models.CASCADE)
+    expert_album_id = models.ForeignKey('songer_admn.Album',on_delete=models.CASCADE,default=None)
+    claps = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.review
